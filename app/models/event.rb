f25a9f58d19c3e142d2cd94aec7a5c3224c78c71
Levy, Adam <Adam.Levy@washpost.com>
2014-03-07 00:30:37 -0500

    adding validation type to paperclip model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   validates :title, presence: true
   has_attached_file :flier
   has_attached_file :flier_image
+  validates_attachment_content_type :flier, :content_type => %w(image/jpeg image/jpg image/png application/pdf)
+  validates_attachment_content_type :flier_image, :content_type => %w(image/jpeg image/jpg image/png)
   
   def self.after_today
     where("event_datetime >= ?", Date.today)
